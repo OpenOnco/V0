c1dc0546f826c6e2d969ee7d4f544677060ed4df
OpenOnco <alexgdickinson@gmail.com>
2025-12-14 18:58:29 -0800

    Add src/data/index.js re-exports

---
+++ src/data/index.js
@@ -0,0 +1,7 @@
+// Re-export all data files for convenient importing
+export { RECENTLY_ADDED_TESTS } from './recentlyAdded';
+export { DATABASE_CHANGELOG } from './changelog';
+export { mrdTestData } from './mrdTests';
+export { ecdTestData } from './ecdTests';
+export { trmTestData } from './trmTests';
+export { tdsTestData } from './tdsTests';
